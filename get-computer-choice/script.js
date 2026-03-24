5c252cc0f25aebe522b01aef04bc4e257b8ecfb8
Arnold Kiritu <arnoldkells4@gmail.com>
2026-03-24 21:49:26 +0300

    created a function to get computers choice in rock, paper and scissors logic the funtion gets a random number that returns rock, paper or scissors

---
+++ get-computer-choice/script.js
@@ -0,0 +1,15 @@
+// Get the computer choice
+
+function getComputerChoice(){
+  let randomChoice = Math.floor(Math.random() * 3);
+
+  if(randomChoice === 0){
+    return "rock";
+  }else if(randomChoice === 1){
+    return "paper";
+  }else{
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
