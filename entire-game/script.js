13695fc78e695afe700929debe204ae970b9bf18
Arnold Kiritu <arnoldkells4@gmail.com>
2026-05-13 21:01:45 +0300

    created a UI for playing the game rock, paper, scissors
    user plays the game against the computer by clicking the buttons and winner determind after five rounds

--- entire-game/script.js
+++ entire-game/script.js
@@ -11,49 +11,97 @@ function getComputerChoice(){
 }
 
 
-function getHumanChoice(){
-  let userInput = prompt("Please enter rock, paper or scissors");
-  if(userInput === null) return "rock";
+// function getHumanChoice(){
+//   let userInput = prompt("Please enter rock, paper or scissors");
+//   if(userInput === null) return "rock";
 
-  return userInput.toLowerCase();
-}
+//   return userInput.toLowerCase();
+// }
+
+let humanScore = 0;
+let computerScore = 0;
+
+// function playGame(){
+  
+
+//   function playRound(humanChoice, computerChoice){
+//     humanChoice = humanChoice.toLowerCase();
+
+//     if(humanChoice === computerChoice){
+//       console.log("Tie, No winner");
+//     }else if(
+//       humanChoice === "rock" && computerChoice === "scissors" ||
+//       humanChoice === "scissors" && computerChoice === "paper" ||
+//       humanChoice === "paper" && computerChoice === "rock"
+//     ){
+//       console.log("You Win! " + humanChoice + " beat " + computerChoice);
+//       humanScore++;
+//     }else{
+//       console.log("You Lose " + computerChoice + " beat " + humanChoice);
+//       computerScore++;
+//     }
+//   }
+
+//   for(let i = 0; i < 5; i++){
+//     const humanSelection = getHumanChoice();
+//     const computerSelection = getComputerChoice();
+//     playRound(humanSelection, computerSelection);
+//   }
+
+//   if(humanScore > computerScore){
+//     console.log("You Won! The game");
+//   }else if(computerScore > humanScore){
+//     console.log("Computer Won! The game");
+//   }else{
+//     console.log("It's a Tie!");
+//   }
+// }
+
+// playGame();
 
 
-function playGame(){
-  let humanScore = 0;
-  let computerScore = 0;
+function playRound(humanChoice, computerChoice){
+  humanChoice = humanChoice.toLowerCase();
 
-  function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
-
-    if(humanChoice === computerChoice){
-      console.log("Tie, No winner");
-    }else if(
-      humanChoice === "rock" && computerChoice === "scissors" ||
-      humanChoice === "scissors" && computerChoice === "paper" ||
-      humanChoice === "paper" && computerChoice === "rock"
-    ){
-      console.log("You Win! " + humanChoice + " beat " + computerChoice);
-      humanScore++;
-    }else{
-      console.log("You Lose " + computerChoice + " beat " + humanChoice);
-      computerScore++;
-    }
+  if(humanScore === 5 || computerScore === 5){
+    return;
   }
 
-  for(let i = 0; i < 5; i++){
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-  }
-
-  if(humanScore > computerScore){
-    console.log("You Won! The game");
-  }else if(computerScore > humanScore){
-    console.log("Computer Won! The game");
+  if(humanChoice === computerChoice){
+    console.log("Tie, No winner");
+    document.getElementById("results").textContent = "Tie, No winner";
+  }else if(
+    humanChoice === "rock" && computerChoice === "scissors" ||
+    humanChoice === "scissors" && computerChoice === "paper" ||
+    humanChoice === "paper" && computerChoice === "rock"
+  ){
+    console.log("You Win! " + humanChoice + " beat " + computerChoice);
+    humanScore++;
+    document.getElementById("results").textContent = "You won!";
   }else{
-    console.log("It's a Tie!");
+    console.log("You Lose " + computerChoice + " beat " + humanChoice);
+    computerScore++;
+    document.getElementById("results").textContent = "You Lost!";
+  }
+
+  document.getElementById("score").textContent = `You: ${humanScore} | Computer: ${computerScore}`;
+
+  if (humanScore === 5) {
+    document.getElementById("results").textContent = "Human Is The Winner!";
+  }else if(computerScore === 5){ 
+    document.getElementById("results").textContent = "Computer Is The Winner!";
   }
 }
 
-playGame();
+
+document.getElementById("rock").addEventListener("click", ( )=>{
+  playRound("rock", getComputerChoice());
+});
+
+document.getElementById("paper").addEventListener("click", ( )=>{
+  playRound("paper", getComputerChoice());
+});
+
+document.getElementById("scissors").addEventListener("click", ( )=>{
+  playRound("scissors", getComputerChoice());
+});
